074115028539a4169610b81d35562ada15019596
Gregory Haynes <greg@greghaynes.net>
2010-09-12 14:20:45 -0700

    mainwindow shows bot selection on start

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 
+#include <QFileDialog>
+
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -7,5 +9,28 @@ MainWindow::MainWindow(QWidget *parent)
 {
   ui = new Ui::MainWindow();
   ui->setupUi(this);
+  setBotSelected(false);
+  selectPath();
 }
+
+void MainWindow::setBotPath(const QString &path)
+{
+  botPath = path;
+}
+
+void MainWindow::selectPath()
+{
+  QFileDialog *dialog = new QFileDialog(this, "Locate your bot");
+  connect(dialog, SIGNAL(fileSelected(const QString&)),
+          this, SLOT(setBotPath(const QString&)));
+  dialog->show();
+}
+
+void MainWindow::setBotSelected(bool value)
+{
+  ui->usernameLineEdit->setEnabled(value);
+  ui->playGamePushButton->setEnabled(value);
+}
+
+#include "mainwindow.moc"
 
