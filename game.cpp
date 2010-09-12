2ae69f3063d789bd7820c34fb4c4b27d06e82150
Gregory Haynes <greg@greghaynes.net>
2010-09-12 15:24:23 -0700

    Added process and connected process signals

--- game.cpp
+++ game.cpp
@@ -1,17 +1,40 @@
 #include "game.h"
 
-
-Game::Game(QObject *parent)
+Game::Game(const QString &hostname,
+      quint16 port,
+      const QString &username,
+      const QString &botPath,
+      QObject *parent)
   : QObject(parent)
 {
+  m_username = username;
+  m_hostname = hostname;
+  m_port = port;
+  m_botPath = botPath;
+
   m_socket = new QTcpSocket(this);
+  connect(m_socket, SIGNAL(connected()),
+    this, SLOT(connected()));
+  connect(m_socket, SIGNAL(error(QAbstractSocket::SocketError socketError)),
+    this, SLOT(connectionError(QAbstractSocket::SocketError socketError)));
+  connect(m_socket, SIGNAL(readyRead()),
+    this, SLOT(serverResponded()));
+
+  m_process = new QProcess(this);
+  m_process->setProcessChannelMode(QProcess::SeparateChannels);
+  m_process->setReadChannel(QProcess::StandardError);
+  connect(m_process, SIGNAL(readyRead()),
+    this, SLOT(botResponded()));
+  connect(m_process, SIGNAL(started()),
+    this, SLOT(botStarted()));
+  connect(m_process, SIGNAL(error(QProcess::ProcessError error)),
+    this, SLOT(botError(QProcess::ProcessError error)));
 }
 
-void Game::play(const QString &path,
-  const QString &hostname,
-  quint16 port)
+void Game::play()
 {
-  m_socket->connectToHost(hostname, port);
+  
+  m_socket->connectToHost(m_hostname, m_port);
 }
 
 QTcpSocket *Game::socket()
@@ -21,9 +44,29 @@ QTcpSocket *Game::socket()
 
 void Game::connected()
 {
+  if(m_username.isEmpty())
+    emit(error(NO_USERNAME));
+
+
 }
 
 void Game::connectionError(QAbstractSocket::SocketError socketError)
+{
+}
+
+void Game::botStarted()
+{
+}
+
+void Game::botError(QProcess::ProcessError error)
+{
+}
+
+void Game::botResponded()
+{
+}
+
+void Game::serverResponded()
 {
 }
 
--- game.h
+++ game.h
@@ -2,7 +2,9 @@
 #define GAME_H
 
 #include <QObject>
+#include <QString>
 #include <QTcpSocket>
+#include <QProcess>
 
 class Game
   : public QObject
@@ -10,20 +12,39 @@ class Game
   Q_OBJECT
 
   public:
-    Game(QObject *parent = 0);
+    enum Error {
+      NO_USERNAME,
+      BAD_BOT_PATH,
+    };
 
-    void play(const QString &path,
-      const QString &hostname,
-      quint16 port);
+    Game(const QString &hostname,
+      quint16 port,
+      const QString &username,
+      const QString &botPath,
+      QObject *parent = 0);
+
+    void play();
 
     QTcpSocket *socket();
+
+  Q_SIGNALS:
+    void error(Game::Error e);
 
   private Q_SLOTS:
     void connected();
     void connectionError(QAbstractSocket::SocketError socketError);
+    void botStarted();
+    void botError(QProcess::ProcessError error);
+    void botResponded();
+    void serverResponded();
 
   private:
     QTcpSocket *m_socket;
+    QProcess *m_process;
+    QString m_username;
+    QString m_botPath;
+    QString m_hostname;
+    quint16 m_port;
 
 };
 
