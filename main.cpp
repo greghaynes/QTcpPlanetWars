69a2b366d399e7e25898b36027205ae51061f5b9
Gregory Haynes <greg@greghaynes.net>
2010-09-12 13:34:56 -0700

    Open mainwindow from ui on exec

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+#include "mainwindow.h"
+
 #include <QApplication>
 
 int main(int argc, char **argv) {
+  QApplication app(argc, argv);
+  MainWindow *m = new MainWindow();
+  m->show();
+  return app.exec();
 }
 
